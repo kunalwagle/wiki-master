62842a1e9464ca81b3b9a4a515aeabd9ac5717a4
grobgl <grobgl@gmail.com>
2015-05-24 21:44:52 +0100

    Added pg & database endpoints to index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/wikimasterdb';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
