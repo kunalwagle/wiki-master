9b78b15983f27909868eb94259fd8102c1684313
grobgl <grobgl@gmail.com>
2015-05-24 21:55:48 +0100

    Added create user endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,3 +9,40 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+/* CREATE /api/v1/wikimaster Create a single user */
+router.post('/api/v1/wikimaster', function(req, res) {
+
+    var results = [];
+
+    // Grab data from http request
+    var data = {username: req.body.username, password: req.body.password, score: 0};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO users(username, password, score) values($1, $2, $3)", [data.username, data.password, data.score]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM users ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+});
