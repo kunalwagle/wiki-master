0177c4537b103bf07c35fc46f7efed92b277884a
grobgl <grobgl@gmail.com>
2015-05-24 22:23:12 +0100

    Added read users endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -46,3 +46,34 @@ router.post('/api/v1/wikimaster', function(req, res) {
 
     });
 });
+
+/* READ /api/v1/wikimaster Get all users */
+router.get('/api/v1/wikimaster', function(req, res) {
+
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM users ORDER BY id ASC;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            client.end();
+            return res.json(results);
+        });
+
+        // Handle Errors
+        if(err) {
+          console.log(err);
+        }
+
+    });
+
+});
