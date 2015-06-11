3fdd75d8c2426bfc1a7a3e5bd7caa36958d0185b
Kunal Wagle <kunal.wagle13@imperial.ac.uk>
2015-06-11 11:48:54 +0100

    Minor clean up for the report

---
+++ WikiMaster/WikiMaster/TopicHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopicHomeViewController.h
+//  WikiMaster
+//
+//  Created by Kunal Wagle on 07/06/2015.
+//  Copyright (c) 2015 WEBAPPS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicHomeViewController : UIViewController
+
+@end
---
+++ WikiMaster/WikiMaster/TopicHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  TopicHomeViewController.m
+//  WikiMaster
+//
+//  Created by Kunal Wagle on 07/06/2015.
+//  Copyright (c) 2015 WEBAPPS. All rights reserved.
+//
+
+#import "TopicHomeViewController.h"
+
+@interface TopicHomeViewController ()
+
+@end
+
+@implementation TopicHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
